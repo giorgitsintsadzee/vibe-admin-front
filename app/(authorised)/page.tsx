97db15b175a690e8c3fcbac5b95e0f481ea649b5
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-09-11 19:34:58 +0400

    create button

--- app/(authorised)/page.tsx
+++ app/(authorised)/page.tsx
@@ -1,12 +1,13 @@
 import AlbumCard from '../Components/AlbumCard/AlbumCard';
 import ArtistCard from '../Components/ArtistCard/ArtistCard';
+import Button from '../Components/Button/Button';
 import NavBarMenu from '../Components/NavBarMenu/NavBarMenu';
 import styles from './page.module.css'
 
 export default function Home() {
     return (
         <>
-            
+        
         </>
     );
 }
