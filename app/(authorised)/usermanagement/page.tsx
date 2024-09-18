311c5ef984a52a4e6f08f905b7997b966df04627
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-18 18:50:09 +0400

    made usermanagment page

---
+++ app/(authorised)/usermanagement/page.tsx
@@ -0,0 +1,11 @@
+import ArtistTable from '@/app/Components/ArtistTable/ArtistTable'
+import styles from './page.module.css'
+
+const usermanagementPage = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default usermanagementPage
