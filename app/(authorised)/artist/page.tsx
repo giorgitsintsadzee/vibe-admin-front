2f3b6b5986a9e50f2761d74727bd0f02044f1a5e
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-18 18:11:35 +0400

    make as a page

---
+++ app/(authorised)/artist/page.tsx
@@ -0,0 +1,10 @@
+import ArtistTable from "@/app/Components/ArtistTable/ArtistTable"
+import styles from './page.module.css'
+
+const ArtistPage = () => {
+return (
+    <ArtistTable/>
+)
+}
+
+export default ArtistPage
