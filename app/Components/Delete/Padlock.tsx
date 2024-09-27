a0f9f8e7559b0e179c5835c427e3822072a7805d
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-27 21:05:52 +0400

    had mistakes

--- app/Components/Delete/Padlock.tsx
+++ app/Components/Delete/Padlock.tsx
@@ -1,23 +1,50 @@
-'use client'
-import { useState } from 'react'
+'use client';
+import { useState } from 'react';
+import axios from 'axios';
 
-const Padlock = () => {
-    const [isLocked, setIsLocked] = useState(false)
+type Props = {
+    id: number;
+};
 
-    const handleClick = () => {
-        setIsLocked(!isLocked) 
-    }
+const Padlock = ({ id }: Props) => {
+    const [isLocked, setIsLocked] = useState(false);
+
+    const handleClick = async () => {
+        try {
+            setIsLocked(!isLocked);
+            const token = document.cookie
+                .split('; ')
+                .find((row) => row.startsWith('token='))
+                ?.split('=')[1];
+
+            if (!token) {
+                throw new Error('No token found');
+            }
+
+
+            await axios.delete(`https://vibetunes-backend.onrender.com/users/${id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+
+            });
+
+        } catch (error) {
+            console.error('Failed to update lock status:', error);
+        }
+    };
 
     return (
         <div onClick={handleClick}>
-            <img 
-                src={isLocked ? "/icons/Lock.svg" : "/icons/Unlock.svg"} 
-                alt={isLocked ? "lock" : "unlock"} 
-                width={32} 
-                height={32} 
+            <img
+                src={isLocked ? '/icons/Lock.svg' : '/icons/Unlock.svg'}
+                alt={isLocked ? 'lock' : 'unlock'}
+                width={32}
+                height={32}
             />
         </div>
-    )
-}
+    );
+};
 
-export default Padlock
+export default Padlock;
