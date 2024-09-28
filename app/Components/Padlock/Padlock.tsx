8958be8de1b57be780d86a4886a7f5e8f2f985d0
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-28 14:20:53 +0400

    made padlock done

--- app/Components/Padlock/Padlock.tsx
+++ app/Components/Padlock/Padlock.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 type Props = {
@@ -7,12 +7,39 @@ type Props = {
 };
 
 const Padlock = ({ id }: Props) => {
-  const [isLocked, setIsLocked] = useState(false);
+  const [isLocked, setIsLocked] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchUserStatus = async () => {
+      try {
+        const token = document.cookie
+          .split('; ')
+          .find((row) => row.startsWith('token='))
+          ?.split('=')[1];
+
+        if (!token) {
+          throw new Error('No token found');
+        }
+
+        const response = await axios.get(`https://vibetunes-backend.onrender.com/users/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const { deletedAt } = response.data;
+
+        setIsLocked(deletedAt !== null);
+      } catch (error) {
+        console.error('Failed to fetch user status:', error);
+      }
+    };
+
+    fetchUserStatus();
+  }, [id]);
 
   const handleClick = async () => {
     try {
-      setIsLocked(!isLocked);
-
       const token = document.cookie
         .split('; ')
         .find((row) => row.startsWith('token='))
@@ -22,20 +49,21 @@ const Padlock = ({ id }: Props) => {
         throw new Error('No token found');
       }
 
-      if (!isLocked) {
-        await axios.delete(`https://vibetunes-backend.onrender.com/users/${id}`, {
-          headers: {
-            // 'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        });
-      } else {
-        
-      }
+      await axios.delete(`https://vibetunes-backend.onrender.com/users/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setIsLocked(!isLocked);
     } catch (error) {
       console.error('Failed to update lock status:', error);
     }
   };
+
+  if (isLocked === undefined) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div onClick={handleClick}>
